00af25db0259b55e8dc15c7f9768bee538abe500
erislandybp <erislandybp@gmail.com>
2022-05-01 12:03:19 -0400

    FormComponent cierra luego de error o exito

--- vue/src/components/ModalFormTrabajadorComponent.vue
+++ vue/src/components/ModalFormTrabajadorComponent.vue
@@ -156,6 +156,7 @@
                       </div>
 
                       <button
+                        @click="$emit('clicked', false)"
                         type="submit"
                         class="w-full mt-9 text-white bg-red-800 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
                       >
